22db030f0b641f38c1ef243983161c68d2e4cc46
kloumpt <serge.molina+git@kloumpt.net>
2017-05-27 23:56:49 +0200

    testing post commit

--- setup.sh
+++ setup.sh
@@ -4,7 +4,5 @@ sudo apt-get install git python3-pip python3-pillow python3-pip
 
 hook_file='.git/hooks/post-commit'
 echo '#!/bin/bash' > "$hook_file"
-echo 'echo Running $BASH_SOURCE' > "$hook_file"
-echo 'set | egrep GIT' > "$hook_file"
-echo 'echo PWD is $PWD' > "$hook_file"
+echo 'touch /root/GPN17_PF/post_commit_successful' >> "$hook_file"
 
