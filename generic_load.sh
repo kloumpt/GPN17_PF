795c391da0174d3777b215cea6d6c7fe2f014287
root <root@7dd18f2b56a5>
2017-05-27 16:25:07 +0000

    cleanups

--- generic_load.sh
+++ generic_load.sh
@@ -2,6 +2,8 @@ PID=$$
 
 kill_childs() {
     killall python3
+    kill -9 $PID
+    exit
 }
 
 trap 'kill_childs' SIGINT SIGTERM
@@ -10,18 +12,12 @@ image_load="$1"
 xoffset="$2"
 yoffset="$3"
 
-cores=64
+cores=128
 for i in `seq 1 $cores`; do
-    nohup python3 pixelflood.py "$image_load" 1000 0 16&
+    nohup python3 pixelflood.py "$image_load" $xoffset $yoffset 16&
 done
 
-echo $!
+while 1;do;done
 
-ps -axf | grep $PID
-
-for pid in `seq 1 $cores`; do
-    echo $pid
-    #wait $pid
-done
 
 
