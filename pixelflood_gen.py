171d8e356bf1d7f396d27681c8ce5d2e9d9b67d7
kloumpt <serge.molina+git@kloumpt.net>
2017-05-27 21:27:38 +0200

    cfg

--- pixelflood_gen.py
+++ pixelflood_gen.py
@@ -16,24 +16,27 @@ yoffset = int(sys.argv[3])
 iterations = int(sys.argv[4])
 path = "temp"
 
-def pixel(myfile, x, y, r, g, b, a=255):
+def pixel(x, y, r, g, b, a=255):
         if a == 255:
             return ('PX %d %d %02x%02x%02x\n' % (x, y, r, g, b))
         elif a > 0:
             return ('PX %d %d %02x%02x%02x%02x\n' % (x, y, r, g, b, a))
         return ''
 
+def gen(mask, x, y, xoffset, yoffset, i):
+        if mask[x, y] > 10:
+            pass#return ''
+        #print(i, ':', x, '/', w, y, '/', h)
+        r, g, b, a = image.getpixel((x, y))
+        return pixel(x + xoffset, y + yoffset, r, g, b, a)
+
+
 def calculate(xoffset, yoffset, image, w, h, i=0):
+    print(i)
     with open(path, "a") as myfile:
         buffer=''
         mask = np.random.randint(0, 100, (w, h))
-        for x in range(w):
-            for y in range(h):
-                if mask[x, y] > 10:
-                    pass#continue
-                print(i, ':', x, '/', w, y, '/', h)
-                r, g, b, a = image.getpixel((x, y))
-                buffer += pixel(myfile, x + xoffset, y + yoffset, r, g, b, a)
+        buffer = ''.join([''.join([gen(mask, x, y, xoffset, yoffset, i) for x in range(w)]) for y in range(h)])
         myfile.write(buffer)
 
 image = Image.open(sys.argv[1]).convert('RGBA')
