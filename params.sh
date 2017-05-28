e66706fc164a5657e42cd0e62913334100e68496
kloumpt <serge.molina+git@kloumpt.net>
2017-05-28 02:09:52 +0200

    cfg

--- params.sh
+++ params.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-image="macron.jpg"
+image="melenchon.jpg"
 xoffset=0
 yoffset=0
 
--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install git python3-pip python3-pillow python3-pip python3-numpy nload
-
+touch temp
 #./git-notify
